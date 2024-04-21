1321f589c229f46559421f4113dbddb4491b7856
Jan Kneissler <jan.kneissler@breuninger.de>
2024-04-21 21:32:01 +0200

    go 1.22.2

--- build-and-push.sh
+++ build-and-push.sh
@@ -3,8 +3,8 @@ echo "============="
 echo "Installing go"
 echo "============="
 echo ""
-wget https://dl.google.com/go/go1.21.linux-amd64.tar.gz
-sudo tar -xvf go1.21.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
+sudo tar -xvf go1.22.2.linux-amd64.tar.gz
 sudo mv go /usr/local
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
