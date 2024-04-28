b2f634bd815544fe003fe00d107030677dd553a5
Jan Kneissler <jan.kneissler@breuninger.de>
2024-04-28 21:45:01 +0200

    split off installation from build-and-push.sh

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,37 +1,5 @@
-GO_VERSION=1.22.2
-BUNDLE_VERSION=v0.0.9
-echo ""
-echo "============="
-echo "Installing go"
-echo "============="
-echo ""
-echo "Go version:" $GO_VERSION
-wget -nv https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz
-sudo tar -xvf go$GO_VERSION.linux-amd64.tar.gz > /dev/null
-sudo mv go /usr/local
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-export GOBIN=/usr/local/go/bin/
-go version
-which go
-echo ""
-echo "===================="
-echo "Checking out sources"
-echo "===================="
-echo ""
-git clone https://github.com/operator-framework/operator-sdk
-cd operator-sdk
-git checkout master
-echo ""
-echo "====================="
-echo "Building operator-sdk"
-echo "====================="
-echo ""
-sudo chmod a+w+x $GOBIN
-ls -l $GOBIN
-make install
-cd ..
+# call install script
+sh scripts/install.sh
 echo ""
 echo "====================="
 echo "Init operator        "
@@ -72,14 +40,6 @@ echo "======================="
 echo "Deploy to test-cluster "
 echo "======================="
 echo ""
-echo $SERVICE_ACCOUNT_JSON_KEY > key.json
-echo "Json key has" `grep -c "" key.json` "lines"
-gcloud auth activate-service-account github-ci-cd@k-pipe-test-system.iam.gserviceaccount.com --key-file=key.json --project=k-pipe-test-system
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt update
-sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin kubectl
-export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 gcloud  container clusters get-credentials k-pipe-runner --region europe-west3
 make deploy IMG="kpipe/operator-bundle:$BUNDLE_VERSION"
 
