f04da54ff2cd7e3ab956b41ee45f0347b6b9d5e1
Jan Kneissler <jan.kneissler@breuninger.de>
2024-05-02 15:23:04 +0200

    working local builder chain most in docker

--- develop-locally/run.sh
+++
@@ -1,7 +0,0 @@
-rm -rf operator
-docker run -it --platform linux/amd64 \
- -v $PWD/operator:/operator \
- -v ~/.config/gcloud:/root/.config/gcloud \
- controller-dev
-
-# -v /var/run/docker.sock:/var/run/docker.sock -v ~/.config/gcloud:/root/.config/gcloud test
