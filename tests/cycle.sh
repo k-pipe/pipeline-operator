fb573b8a5e7c4f338f8bc8d43e12db1633699f25
k-pipe <k-pipe@k-pipe.cloud>
2024-05-14 15:47:50 +0000

    version 0.1.40

--- tests/cycle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo uninstall chart
-helm uninstall k-pipe -n k-pipe
-echo remove repo
-helm repo remove k-pipe
-echo add repo
-helm repo add k-pipe https://k-pipe.github.io/pipeline-operator/
-echo install chart
-helm install k-pipe k-pipe/pipeline-controller -n k-pipe
