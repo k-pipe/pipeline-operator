1dc9021dc94132b90bdd83930cb16d6c48fa4004
cicd <cicd@k-pipe.cloud>
2024-05-19 17:16:07 +0000

    delete conntroller deployment for local developlment

--- source/tests/cycle.sh
+++ source/tests/cycle.sh
@@ -7,3 +7,4 @@ echo add repo
 helm repo add k-pipe https://k-pipe.github.io/pipeline-operator/
 echo install chart
 helm install k-pipe k-pipe/pipeline-controller -n k-pipe
+kubectl delete deployment k-pipe-pipeline-controller -n k-pipe
