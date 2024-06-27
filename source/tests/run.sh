45d8954aae360484b8efddcd043ebbc8daba45fd
cicd <cicd@k-pipe.cloud>
2024-06-27 19:50:07 +0000

    use config map, nodeselector, repoClass dependent label (for network policy), refactor pipeline definition reconcile method, err in message

--- source/tests/run.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete -f pipeline-return-forecast.yaml
-kubectl delete -f run-return-forecast.yaml
-kubectl apply -f pipeline-return-forecast.yaml
-kubectl apply -f run-return-forecast.yaml
